f45bacf4063e975a81dad42f8f3feda90fd43a6e
Aditya Earanky <aearanky@terpmail.umd.edu>
2018-07-20 01:31:33 -0400

    convert to classes

--- src/app/top-renderer/top-renderer.component.ts
+++ src/app/top-renderer/top-renderer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import * as MRCLoader from '../../lib/loaders/MRCLoader';
 
 @Component({
   selector: 'app-top-renderer',
@@ -7,9 +9,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopRendererComponent implements OnInit {
 
-  constructor() { }
+  private renderer3;
+  private camera3;
+  private dirLight = new THREE.DirectionalLight(0xffffff);
+  private windowWidth;
+  private windowHeight;
+  private containerX;
+  private sceneX;
+
+  private twoHeight = 200;
+  private twoWidth = 200;
+  private view;
+  public views;
+
+  constructor() {
+
+    this.views = [
+      {
+        left: 0.6,
+        bottom: 0.70,
+        width: 0.3,
+        height: 0.233,
+        background: new THREE.Color().setRGB(0, 0, 0),
+        eye: [-300, 0, 0],
+        up: [0, 1, 0],
+        fov: 60,
+
+        updateCamera: function (camera3, sceneX) {
+          camera3.lookAt(sceneX.position);
+        }
+      }
+    ];
+  }
 
   ngOnInit() {
+    this.dirLight = new THREE.DirectionalLight(0xffffff);
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
+    this.init();
+    this.animate();
   }
+
+  init() {
+
+    this.view = this.views[0];
+    this.camera3 = new THREE.PerspectiveCamera(this.view.fov, this.twoWidth / this.twoHeight, 0.01, 10000);
+    this.camera3.position = [this.view.eye[0] + 128, this.view.eye[1], this.view.eye[2]];
+    this.camera3.up = [this.view.up[0], this.view.up[1], this.view.up[2]];
+
+    this.sceneX = new THREE.Scene();
+
+    this.dirLight.position.set(200, 200, 1000).normalize();
+    this.sceneX.add(this.dirLight);
+    const manager = new THREE.LoadingManager();
+    const loader = new MRCLoader(manager);
+
+    console.log('Manager is ready');
+
+    loader.load('models/mrc/bin8Data/avebin8.mrc', function (volume) {
+      const geometry = new THREE.BoxGeometry(volume.xLength, volume.yLength, volume.zLength);
+      const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+      const cubeX = new THREE.Mesh(geometry, material);
+      cubeX.visible = false;
+      this.sceneX.add(this.cubeX);
+    });
+
+    this.renderer3 = new THREE.WebGLRenderer({alpha: true});
+    this.containerX = document.createElement('containerX');
+    document.body.appendChild(this.containerX);
+    this.containerX.appendChild(this.renderer3.domElement);
+
+  }
+
+  animate() {
+    this.setupRenderer();
+    requestAnimationFrame(this.animate);
+
+  }
+
+  setupRenderer() {
+    this.containerX = document.getElementById('containerX');
+    this.renderer3.setPixelRatio(window.devicePixelRatio);
+    this.renderer3.setSize(this.twoWidth, this.twoHeight);
+    this.containerX.appendChild(this.renderer3.domElement);
+    this.view = this.views[0];
+    this.view.updateCamera(this.camera3, this.sceneX);
+    this.camera3.aspect = this.twoWidth / this.twoHeight;
+    this.camera3.lookAt(this.sceneX.position);
+    this.renderer3.render(this.sceneX, this.camera3);
+
+  }
+
 
 }
